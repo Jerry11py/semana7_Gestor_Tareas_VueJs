d32c39e6abe1af24ca168bfadceeb4d8ca342332
Jerry11py <jerry10py@gmail>
2024-10-29 21:33:21 -0300

    Modificacion para entrega de GitHubPages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/semana7_Gestor_Tareas_VueJs/' // Reemplaza el nombre exacto de tu repositorio
+  : '/'
+});
